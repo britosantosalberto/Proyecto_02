e2e31a00a2ad77af587c8576821f8e6815fe5594
britosantosalberto <91129234+britosantosalberto@users.noreply.github.com>
2021-10-17 23:23:16 +0200

    Add files via upload

---
+++ src/inserts02.js
@@ -0,0 +1,11 @@
+db.futbolistas.insertMany([
+    {futbolista:"cristiano", posición:"DC", edad:"36"},
+    {futbolista:"messi", posición:"DC", edad:"34"},
+    {futbolista:"mbappe", posición:"DC", edad:"22"},
+    {futbolista:"ramos", posición:"DFC", edad:"35"},
+    {futbolista:"casemriro", posición:"MC", edad:"29"},
+    {futbolista:"de gea", posición:"POR", edad:"30"},
+    {futbolista:"de bruyne", posición:"MC", edad:"30"},
+    {futbolista:"kimpembe", posición:"DFC", edad:"26"},
+    {futbolista:"modric", posición:"MC", edad:"36"},
+])
